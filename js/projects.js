0537c83cd06031894238158df8faad91d0e7a3d5
mattw <mattwayles@gmail.com>
2017-03-22 12:43:06 -0400

    Organized files into folders. Ready for change implementation.

---
+++ js/projects.js
@@ -0,0 +1,25 @@
+$("#prev-btn").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".carousel-c").offset().top},
+        'slow');
+});
+$("#next-btn").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".carousel-c").offset().top},
+        'slow');
+});
+$("#prev-btn2").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".carousel-c").offset().top},
+        'slow');
+});
+$("#next-btn2").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".carousel-c").offset().top},
+        'slow');
+});
+$(".carousel-indicators").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".carousel-c").offset().top},
+        'slow');
+});
